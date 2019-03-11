32c5034912e822830a09ec21f1618c979c2ae595
RasmusAgergaard <rasmusap@gmail.com>
2019-03-11 09:31:10 +0100

    Created

---
+++ Features/MyLinq.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    public static class MyLinq
+    {
+        public static int Count<T>(IEnumerable<T> sequence)
+        {
+            return 1;
+        }
+    }
+}
