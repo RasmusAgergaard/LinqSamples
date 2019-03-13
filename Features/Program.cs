275780783f05f3f9a829d1edb771370714d6264a
RasmusAgergaard <rasmusap@gmail.com>
2019-03-13 08:05:08 +0100

    Querys and Deferred Execution

--- Features/Program.cs
+++ Features/Program.cs
@@ -10,19 +10,26 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            IEnumerable<Employee> developers = new Employee[]
+            var developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Scott"},
                 new Employee {Id = 2, Name = "Chris"}
             };
 
-            IEnumerable<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
                 new Employee{Id = 3, Name = "Alex"}
             };
 
-            foreach (var employee in developers.Where(e => e.Name.Length == 5)
-                                               .OrderBy(e => e.Name))
+            var query = developers.Where(e => e.Name.Length == 5)
+                                  .OrderBy(e => e.Name);
+
+            var query2 = from developer in developers
+                         where developer.Name.Length == 5
+                         orderby developer.Name
+                         select developer;
+
+            foreach (var employee in query2)
             {
                 Console.WriteLine(employee.Name);
             }
