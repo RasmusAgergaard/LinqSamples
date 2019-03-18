c1f9d97763a2f1b89008a30d1798248e7018da70
RasmusAgergaard <rasmusap@gmail.com>
2019-03-18 07:57:24 +0100

    New file added

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -11,32 +11,32 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = ProcessFile("fuel.csv");
+            var cars = ProcessCars("fuel.csv");
+            var manufactures = ProcessManufactures("manufacturers.csv");
 
             //Query syntax
             var query =
                 from car in cars
-                where car.Manufacturer == "BMW" && car.Year == 2016
+                join manufacture in manufactures on car.Manufacturer equals manufacture.Name
                 orderby car.Combined descending, car.Name ascending
-                select car;
+                select new
+                {
+                    manufacture.Headquarters,
+                    car.Name,
+                    car.Combined
+                };
 
-            //Extension method syntax with lambda expression
-            var top =
-                cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-                .OrderByDescending(c => c.Combined)
-                .ThenBy(c => c.Name)
-                .Select(c => c)
-                .First();
-                
+
+
 
             foreach (var car in query.Take(10))
             {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
+                Console.WriteLine($"{car.Headquarters} {car.Name} : {car.Combined}");
             }
 
         }
 
-        private static List<Car> ProcessFile(string path)
+        private static List<Car> ProcessCars(string path)
         {
             return
                 File.ReadAllLines(path)
@@ -44,6 +44,24 @@ namespace Cars
                     .Where(line => line.Length > 1)
                     .Select(Car.ParseFromCsv)
                     .ToList();
+        }
+
+        private static List<Manufacture> ProcessManufactures(string path)
+        {
+            var query =
+                File.ReadAllLines(path)
+                    .Where(l => l.Length > 1)
+                    .Select(l =>
+                    {
+                        var columns = l.Split(',');
+                        return new Manufacture
+                        {
+                            Name = columns[0],
+                            Headquarters = columns[1],
+                            Year = int.Parse(columns[2])
+                        };
+                    });
+            return query.ToList();
         }
 
     }
